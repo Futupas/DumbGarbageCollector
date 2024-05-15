1c41bf1668e345905ca56f1e228f2ca412920f09
Oleksandr Pascal <futupas@gmail.com>
2024-05-15 09:28:36 +0000

    upd: always check bad pointers

--- main.c
+++ main.c
@@ -6,6 +6,14 @@
 
 // Usage example
 
+static void assert_pointer_is_not_null(struct dumb_gc* dgc, void* ptr) {
+    if (ptr != NULL) return;
+
+    fprintf(stderr, "Error [re]allocating memory\n");
+    dgc_free_all(dgc); // Don't forget to free the resources!
+    exit(EXIT_FAILURE);
+}
+
 int main(void)
 {
     // Init DGC
@@ -13,10 +21,12 @@ int main(void)
 
     // CAlloc example
     int32_t* ints1 = dgc_calloc(dgc, sizeof(int32_t), 100);
+    assert_pointer_is_not_null(dgc, ints1);
     printf("CAlloc: %d, %d\n", ints1[4], ints1[20]);
 
     // MAlloc example
     int32_t* ints2 = dgc_malloc(dgc, sizeof(int32_t) * 100);
+    assert_pointer_is_not_null(dgc, ints2);
     ints2[4] = 6;
     ints2[20] = 9;
     printf("MAlloc: %d, %d\n", ints2[4], ints2[20]);
@@ -27,12 +37,7 @@ int main(void)
 
     // ReAlloc example
     ints1 = dgc_realloc(dgc, ints1, sizeof(int32_t) * 200);
-    if (ints1 == NULL) {
-        fprintf(stderr, "Error reallocating memory\n");
-        // Don't forget to free the resources!
-        dgc_free_all(dgc);
-        exit(EXIT_FAILURE);
-    }
+    assert_pointer_is_not_null(dgc, ints1);
     ints1[104] = 6;
     ints1[120] = 9;
     printf("ReAlloc: %d, %d\n", ints1[104], ints1[120]);
