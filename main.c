b28d316d7b0a9d66f0ac6f593ef12dbd30063c82
Oleksandr Pascal <futupas@gmail.com>
2024-06-02 18:27:43 +0000

    add: lil comment

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ int main(void)
         printf("Add existing pointer: OK\n");
     }
     {
-        int* ptr = malloc(5);
+        int* ptr = malloc(5); // it can be `strdup` or something that allocates memory inside
         assert_pointer_is_not_null(dgc, ptr);
         int res = dgc_add(dgc, ptr);
         assert_equals(dgc, 1, res);
